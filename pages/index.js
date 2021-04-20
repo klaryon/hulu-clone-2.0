d6764585da535deb6e052199da4a42c85870a183
klaryon <klary.ardon@gmail.com>
2021-04-20 17:36:55 +0200

    title modified

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,13 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>Let's build Hulu 2.0</h1>
+      {/* Header */}
+      {/* Navbar  */}
+      {/* Results  */}
     </div>
   )
 }
