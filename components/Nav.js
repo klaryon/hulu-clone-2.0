c3719c3466dba36f942028ac397665a5d0e01178
klaryon <klary.ardon@gmail.com>
2021-04-21 14:55:06 +0200

    gradient fade nav scroll

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import requests from "../utils/requests";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
@@ -13,6 +13,7 @@ const Nav = () => {
           </h2>
         ))}
       </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"/>
     </nav>
   );
 };
