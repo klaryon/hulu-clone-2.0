dc824ebbf845732d8a29a5fd3c29dfe933c31d7d
klaryon <klary.ardon@gmail.com>
2021-04-21 15:54:24 +0200

    results component styled

--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,7 @@ import Thumbnail from "./Thumbnail";
 
 function Results({ results }) {
   return (
-    <div>
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
