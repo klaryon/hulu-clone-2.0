92fb502658a2b4291f8deb64c8b088d8faffc9ad
klaryon <klary.ardon@gmail.com>
2021-04-21 14:49:26 +0200

    tailwind-scrollbar-hide

--- components/Nav.js
+++ components/Nav.js
@@ -3,9 +3,14 @@ import requests from "../utils/requests";
 const Nav = () => {
   return (
     <nav>
-      <div>
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
-          <h2>{title}</h2>
+          <h2
+            key={key}
+            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>
         ))}
       </div>
     </nav>
