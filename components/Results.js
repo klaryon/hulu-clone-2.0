fc3eb1201d2461a85404a631ce289438e73ff609
klaryon <klary.ardon@gmail.com>
2021-04-21 15:26:18 +0200

    thumbnail component created

--- components/Results.js
+++ components/Results.js
@@ -1,9 +1,13 @@
-function Results() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Thumbnail from "./Thumbnail";
+
+function Results({ requests }) {
+  return (
+    <div>
+      {requests.map((result) => (
+        <Thumbnailbnail />
+      ))}
+    </div>
+  );
 }
 
-export default Results
+export default Results;
---
+++ components/Thumbnail.js
@@ -0,0 +1,9 @@
+const Thumbnail = () => {
+    return (
+        <div>
+            This is a Thumbnail
+        </div>
+    )
+}
+
+export default Thumbnail
