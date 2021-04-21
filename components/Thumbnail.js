346e458d1ac9d7a1411aabd2ccf868c43e628758
klaryon <klary.ardon@gmail.com>
2021-04-21 16:06:30 +0200

    flip move

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,15 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
+import { forwardRef } from "react";
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-x-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-x-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -28,6 +32,6 @@ const Thumbnail = ({ result }) => {
       </div>
     </div>
   );
-};
+});
 
 export default Thumbnail;
